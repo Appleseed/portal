f2010394accb72d6e16acfc094b9cb86a7bf89a9
ashishpatel <ashish.patel@haptix.biz>
2015-02-16 10:46:49 +0530

    Automatic script installer bug resolved

--- Master/Appleseed/WebSites/Appleseed/Setup/Scripts/2.0.0.1896.sql
+++ Master/Appleseed/WebSites/Appleseed/Setup/Scripts/2.0.0.1896.sql
@@ -1,27 +1,4 @@
-/*Add new module detail*/
-GO 
-
---IF NOT EXISTS (SELECT * FROM [dbo].[rb_ModuleDefinitions] WHERE PortalID = 0 AND GeneralModDefID = 'C1EA4115-E7F2-4CBC-B1E7-DDA46791493C')
---BEGIN																																	
-
---EXEC	[dbo].[rb_AddGeneralModuleDefinitions]
---		@GeneralModDefID = 'C1EA4115-E7F2-4CBC-B1E7-DDA46791493C',
---		@FriendlyName = N'Page Friendly URL',
---		@DesktopSrc = N'DesktopModules/CoreModules/PageFriendlyURL/PageFriendlyURL.ascx',
---		@MobileSrc = N'',
---		@AssemblyName = N'Applesseed.Dll',
---		@ClassName = N'Appleseed.DesktopModules.CoreModules.PageFriendlyURL.PageFriendlyURL',
---		@Admin = 1,
---		@Searchable = 1
---END
---GO
-
-IF NOT EXISTS (SELECT * FROM [dbo].[rb_ModuleDefinitions] WHERE PortalID = 0 AND GeneralModDefID = 'C1EA4115-E7F2-4CBC-B1E7-DDA46791493C')
-BEGIN																																	
-	insert into [dbo].[rb_ModuleDefinitions] (PortalID, GeneralModDefID) values (0,'C1EA4115-E7F2-4CBC-B1E7-DDA46791493C');
-END
-GO
-
+﻿
 /* Add new column FriendlyName into the rb_GeneralModuleDefinitions table*/
 alter table dbo.rb_Pages add FriendlyURL[nvarchar](512) 
 GO
