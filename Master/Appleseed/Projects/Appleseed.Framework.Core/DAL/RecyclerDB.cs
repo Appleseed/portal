39a212bad28a4a9902baaf149a61cafbfc68da1f
ashishpatel <ashish.patel@haptix.biz>
2016-01-04 15:32:27 +0530

    Connection Pool Issue solved

--- Master/Appleseed/Projects/Appleseed.Framework.Core/DAL/RecyclerDB.cs
+++ Master/Appleseed/Projects/Appleseed.Framework.Core/DAL/RecyclerDB.cs
@@ -148,6 +148,7 @@ namespace Appleseed.Framework.Site.Data
                     m.MobileSrc = result["MobileSrc"].ToString();
                     m.Admin = bool.Parse(result["Admin"].ToString());
                 }
+                connection.Close(); //Added by Ashish Patel - Connection pool issue
 
                 return m;
             }
@@ -259,6 +260,11 @@ namespace Appleseed.Framework.Site.Data
                 {
                     ErrorHandler.Publish(
                         LogLevel.Warn, "An Error Occurred in MoveModuleToNewTab. Parameter : " + moduleId, ex);
+                }
+                //Added by Ashish - Connection Pool Issue
+                finally
+                {
+                    connection.Close();
                 }
             }
         }
