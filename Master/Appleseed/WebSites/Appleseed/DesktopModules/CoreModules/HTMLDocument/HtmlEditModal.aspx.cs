455cc23aac442192dd3fd1402d83ca6ed6608bce
ashishpatel <ashish.patel@haptix.biz>
2016-01-04 16:16:09 +0530

    Connection Pool Issue solved

--- Master/Appleseed/WebSites/Appleseed/DesktopModules/CoreModules/HTMLDocument/HtmlEditModal.aspx.cs
+++ Master/Appleseed/WebSites/Appleseed/DesktopModules/CoreModules/HTMLDocument/HtmlEditModal.aspx.cs
@@ -148,6 +148,11 @@ namespace Appleseed.DesktopModules.CoreModules.HTMLDocument
                     }
                 }
             }
+            //Added by Ashish - Connection pool Issue
+            if (drList != null)
+            {
+                drList.Close();
+            }
             text.UpdateHtmlText(
                 this.ModuleID,
                 this.Server.HtmlEncode(this.DesktopText.Text),
