cff27d29e5f8bc6d20f6919a34540eed4a3005c7
Eric Ramseur <ramseur@anant.us>
2016-02-20 12:45:30 -0500

    delete old themes

--- Master/Appleseed/WebSites/Appleseed/Design/Themes/Corporate-Black/js/admin-bar-margin.js
+++
@@ -1,6 +0,0 @@
-// move the page down a little bit because of the admin bar. 
-$(document).ready(function () {
-    if ($('.navbar-admin').css("display") == "block") {
-        $(".DefaultLayoutDiv").css("margin-top", "50px");
-    }
-});
