37e07502d890c0ba858911a9cb3ae5d6022b69d7
Ashish Patel <info.haptix@gmail.com>
2016-05-24 18:51:03 +0530

    Added Script file - 2.0.0.1905.sql

---
+++ Master/Appleseed/WebSites/Appleseed/Setup/Scripts/2.0.0.1905.sql
@@ -0,0 +1,29 @@
+/* CREATE TABLE FOR DYNAMIC PAGE REDIRECTION */
+IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[rb_Pages_DynamicRedirects]') AND type in (N'U'))
+BEGIN
+Create table rb_Pages_DynamicRedirects
+(
+DynamicPageID int identity(1,1) primary key,
+FriendlyUrl nvarchar(512),
+RedirectToUrl nvarchar(512)
+)
+END
+
+/* CREATE PAGE FOR Page Friendly URL if not exist */
+DECLARE @PAGEID INT,
+		@ModuleDefId INT,
+		@ModuleID INT
+if not exists(select * from rb_pages where PageName='PageFriendlyUrl')
+BEGIN
+EXEC rb_AddTab 0,100,'PageFriendlyUrl',50,'Admins;',0,'', @PAGEID output
+END
+
+select @ModuleDefId=ModuleDefID from rb_ModuleDefinitions where GeneralModDefID='C1EA4115-E7F2-4CBC-B1E7-DDA46791493C'
+
+/* Add PageFriendlyUrl Module on new created page if not loaded */
+IF NOT EXISTS(SELECT * FROM rb_Modules WHERE ModuleDefID=@ModuleDefId)
+BEGIN
+EXEC rb_addModule @PAGEID,1,'Page Friendly URL','ContentPane',@ModuleDefId,0,'Admins','Admins;','Admins;','Admins;','Admins;','Admins;','Admins;',0,NULL,0,0,0,@ModuleID output
+END
+
+
