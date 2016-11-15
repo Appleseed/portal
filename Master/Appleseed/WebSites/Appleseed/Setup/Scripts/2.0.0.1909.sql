e4c1b450dd7ed649957eca8f78731f298d802a71
Info.haptix@gmail.com <Info.haptix@gmail.com>
2016-11-15 17:55:37 +0530

    Sign In page changes

---
+++ Master/Appleseed/WebSites/Appleseed/Setup/Scripts/2.0.0.1909.sql
@@ -0,0 +1,9 @@
+﻿IF NOT EXISTS(SELECT * FROM [rb_PortalSettings] WHERE  [SettingName] = 'SITESETTINGS_LOGIN_TYPE')
+BEGIN
+	INSERT INTO [rb_PortalSettings] VALUES(0,'SITESETTINGS_LOGIN_TYPE','~/desktopmodules/coremodules/signin/signincool.ascx')
+END
+ELSE
+BEGIN
+	update [rb_PortalSettings] set [SettingValue] = '~/desktopmodules/coremodules/signin/signincool.ascx' where [SettingName] = 'SITESETTINGS_LOGIN_TYPE'
+END
+go
