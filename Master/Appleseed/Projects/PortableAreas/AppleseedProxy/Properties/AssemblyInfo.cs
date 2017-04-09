ec70a2a1ff0e743e2c318336d5d7379b3983744d
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:00:23 -0400

    update proxy module desc

--- Master/Appleseed/Projects/PortableAreas/AppleseedProxy/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/PortableAreas/AppleseedProxy/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AppleseedProxy")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Appleseed Portal and Content Management System : MVC Proxy Module")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ANANT Corporation")]
 [assembly: AssemblyProduct("AppleseedProxy")]
