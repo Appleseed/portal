f09239ecec801309006994aeccfe5e920d563707
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:13:23 -0400

    update vs for release

--- Master/Appleseed/Projects/Appleseed.Framework.Core/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/Appleseed.Framework.Core/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.98.383")]
-[assembly: AssemblyFileVersion("1.4.98.383")]
+[assembly: AssemblyVersion("1.4.131.463")]
+[assembly: AssemblyFileVersion("1.4.131.463")]
 
 // In order to sign your assembly you must specify a key to use. Refer to the 
 // Microsoft .NET Framework documentation for more information on assembly signing.
