9ddafb64af604e8a9ef0c52fd6157efad1bc2cfa
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:16:49 -0400

    update vs

--- Master/Appleseed/Projects/PortableAreas/SelfUpdater/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/PortableAreas/SelfUpdater/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Web;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.4.98.383")]
-[assembly: AssemblyFileVersion("1.4.98.383")]
+[assembly: AssemblyVersion("1.4.131.463")]
+[assembly: AssemblyFileVersion("1.4.131.463")]
 
 [assembly: PreApplicationStartMethod(typeof(SelfUpdater.Initializer), "Initialize")]
