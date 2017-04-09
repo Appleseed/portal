ed6163d2028bb3ddda141e7ca99501dd74fc4f10
Eric Ramseur <ramseur@anant.us>
2017-04-08 22:58:52 -0400

    update description of what this does

--- Master/Appleseed/Projects/Appleseed.Framework.UrlRewriting/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/Appleseed.Framework.UrlRewriting/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Appleseed.UrlRewriting")]
-[assembly: AssemblyDescription("Appleseed Portal and Content Management System")]
+[assembly: AssemblyDescription("Appleseed Portal and Content Management System : URL Rewriting ")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ANANT Corporation")]
 [assembly: AssemblyProduct("Appleseed Portal")]
