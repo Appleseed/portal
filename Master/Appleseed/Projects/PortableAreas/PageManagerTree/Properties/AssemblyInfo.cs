872a3596355b5a43219121112800e63771d3aabe
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:06:19 -0400

    add of pm fix desc

--- Master/Appleseed/Projects/PortableAreas/PageManagerTree/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/PortableAreas/PageManagerTree/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PageManagerTree")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Appleseed Portal and Content Management System : MVC Page Manager")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ANANT Corporation")]
 [assembly: AssemblyProduct("PageManagerTree")]
