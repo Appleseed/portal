960560eaae6d371bea6c201864fdda9569856efe
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:07:32 -0400

    fix password desc

--- Master/Appleseed/Projects/PortableAreas/Password/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/PortableAreas/Password/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Password")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Appleseed Portal and Content Management System")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ANANT Corporation")]
 [assembly: AssemblyProduct("Password")]
