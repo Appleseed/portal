138dfe84494debb99b4c865295b79ecf2680b60c
Eric Ramseur <ramseur@anant.us>
2017-04-08 23:05:36 -0400

    fix of user manager desc

--- Master/Appleseed/Projects/PortableAreas/UserManager/Properties/AssemblyInfo.cs
+++ Master/Appleseed/Projects/PortableAreas/UserManager/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("UserManager")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Appleseed Portal and Content Management System : MVC User Manager")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ANANT Corporation")]
 [assembly: AssemblyProduct("UserManager")]
