7460768eeb3b0b15f985edb00a2b0688d369d408
Ashish <info.haptix@gmail.com>
2017-06-16 16:36:55 +0530

    Admin Logged-In user menu changes to render as profile dropdown menu - AP

--- Master/Appleseed/WebSites/Appleseed/Design/Themes/Flatly.Bootstrap/js/admin-menu-table-unwrap.js
+++ Master/Appleseed/WebSites/Appleseed/Design/Themes/Flatly.Bootstrap/js/admin-menu-table-unwrap.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
     $('#Admin_asSiteTree_SkipLink').remove();                                                                   //Remove skip link
 
     //Top Right Admin Menu
-    $('#Admin_PortalHeaderMenu>tbody>tr>td:first-child').contents().wrap('<li class="welcome-msg"></li>');      //Wrap the welcome msg
+    $('#Admin_PortalHeaderMenu>tbody>tr>td:first-child').contents().wrap('<li class=""></li>');      //Wrap the welcome msg
     $('#Admin_PortalHeaderMenu>tbody>tr>td').contents().unwrap();                                               //Remove TDs
     $('#Admin_PortalHeaderMenu>tbody>tr').contents().unwrap();                                                  //Remove TRs
     $('#Admin_PortalHeaderMenu>tbody').contents().unwrap();                                                     //Remove TBODY
@@ -21,9 +21,9 @@ $(document).ready(function () {
 
     if (firstChars != "Login")                                                                                  //Checks to see if the firstChars aren't Login
     {
-        $('#Admin_HeaderMenu2>tbody>tr>td:first-child').contents().wrap('<li class="welcome-msg"></li>');       //Wrap the welcome msg if user is logged in
-    }    
-    
+        $('#Admin_HeaderMenu2>tbody>tr>td:first-child').contents().wrap('<li class=""></li>');       //Wrap the welcome msg if user is logged in
+    }
+
     $('#Admin_HeaderMenu2>tbody>tr>td').contents().unwrap();                                                    //Remove TDs
     $('#Admin_HeaderMenu2>tbody>tr').contents().unwrap();                                                       //Remove TRs
     $('#Admin_HeaderMenu2>tbody').contents().unwrap();                                                          //Remove TBODY
@@ -32,4 +32,74 @@ $(document).ready(function () {
     $('#Admin_HeaderMenu2').contents().unwrap();                                                                //Remove the TABLE
     $('.admin-menu-list').attr('id', 'Admin_HeaderMenu2');                                                      //Add the TABLEs ID to the UL
     $('.admin-menu-list').addClass('nav navbar-nav navbar-right');                                              //Add nav classes to right admin navbar
+
+    //wrapping into profile menu
+    if ($("#Admin_HeaderMenu2").html().toLowerCase().indexOf("/logon.aspx") == -1) {
+        
+        //logged in user menu
+        $("#Admin_HeaderMenu2").append("<li id='liAdminProfile' class=\"dropdown\"><a class='SiteLink dropdown-toggle'  data-hover='dropdown' data-toggle=''  href='javascript:;' id='hypProfile'><span>Profile</span><span class='caret'></span></a> <ul id='ulAdminProfileSubMenu' class='dropdown-menu'></ul></li>");
+
+        $("#ulAdminProfileSubMenu").html('');
+        $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[0]);
+        $("#ulAdminProfileSubMenu").append("<li><hr/></li>");
+        var added = 0;
+        for (var i = 0; i < $("#Admin_HeaderMenu2").children().length; i++) {
+            if ($($("#Admin_HeaderMenu2 li")[i]).html().toLowerCase().indexOf("/register.aspx") > -1 && added == 0) {
+               $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[i]);
+                added = 1;
+            }
+        }
+        $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[0]);
+       
+        added = 0;
+        for (var i = 0; i < $("#Admin_HeaderMenu2").children().length; i++) {
+            if ($($("#Admin_HeaderMenu2 li")[i]).html().toLowerCase().indexOf("/memberinvite/renderview") > -1 && added == 0) {
+                $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[i]);
+                added = 1;
+            }
+        }
+        added = 0;
+        for (var i = 0; i < $("#Admin_HeaderMenu2").children().length; i++) {
+            if ($($("#Admin_HeaderMenu2 li")[i]).html().toLowerCase().indexOf("popuplang") > -1 && added == 0) {
+                $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[i]);
+                added = 1;
+            }
+        }
+
+        $("#ulAdminProfileSubMenu").append("<li><hr/></li>");
+
+        added = 0;
+        for (var i = 0; i < $("#Admin_HeaderMenu2").children().length; i++) {
+            if ($($("#Admin_HeaderMenu2 li")[i]).html().toLowerCase().indexOf("/logoff.aspx") > -1 && added == 0) {
+                $("#ulAdminProfileSubMenu").append($("#Admin_HeaderMenu2 li")[i]);
+                added = 1;
+            }
+        }
+        //$("#Admin_HeaderMenu2 li").each(function (idx, itm) {
+        //    if ($(this).html().toLowerCase().indexOf("/register.aspx") > -1) {
+        //        $("#ulAdminProfileSubMenu").append($(this).html());
+        //    }
+        //});
+        /*
+        <li class="dropdown">
+                        <a href="/100" class="dropdown-toggle" data-toggle="" data-hover="dropdown"><span>Administration</span><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+
+                                <li><a href='/240'>Site Settings</a></li>
+                                <li><a href="/110">Page Manager</a></li>
+                                <li><a href="/280">User Manager</a></li>
+                                <li><a href="/155">File Manager</a></li>
+                                <!--<li><a href="/120">Global Modules</a></li>-->
+                                <li><a href="/215">Recycle Bin</a></li>
+
+
+                                <li><a href="/110">Page Manager</a></li>
+                                <li><a href="/155">File Manager</a></li>
+
+                        </ul>
+                    </li>
+        */
+    }
+    else {
+    }
 });
