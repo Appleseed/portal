909904021c9b1dafb2647504d86e828939259055
ashishpatel <ashish.patel@anant.us>
2017-06-19 16:01:49 +0530

    New Upgrade to cleanup administration menu - SS

--- Master/Appleseed/WebSites/Appleseed/Global.asax.cs
+++ Master/Appleseed/WebSites/Appleseed/Global.asax.cs
@@ -470,7 +470,7 @@ namespace Appleseed
             // moved from PortalSettings
             var f = FileVersionInfo.GetVersionInfo(Assembly.GetAssembly(typeof(Portal)).Location);
             HttpContext.Current.Application.Lock();
-            HttpContext.Current.Application["CodeVersion"] = 1921;
+            HttpContext.Current.Application["CodeVersion"] = 1922;
             HttpContext.Current.Application["NugetSelfUpdatesToInstall"] = false;
             HttpContext.Current.Application.UnLock();
 
