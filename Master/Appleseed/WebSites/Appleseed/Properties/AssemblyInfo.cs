b44f67e5457fb4847825096d7cf82a7a2e7cd8cc
Eric Ramseur <ramseur@anant.us>
2017-08-04 11:30:32 -0400

    Update Version : 1.7.171.0 URL Changes + Fix User Manager Delete

--- Master/Appleseed/WebSites/Appleseed/Properties/AssemblyInfo.cs
+++ Master/Appleseed/WebSites/Appleseed/Properties/AssemblyInfo.cs
@@ -31,6 +31,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.6.160.540")]
-[assembly: AssemblyFileVersion("1.6.160.540")]
+[assembly: AssemblyVersion("1.7.171.0")]
+[assembly: AssemblyFileVersion("1.7.171.0")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
